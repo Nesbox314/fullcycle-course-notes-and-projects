df3925b992e07845541389ae5f68e5567e948ea7
Ricardo Antônio Mazzuco <ricardopop96@gmail.com>
2023-11-21 21:27:57 -0300

    client developed

---
+++ Projects/WalletCore/internal/entity/client_test.go
@@ -0,0 +1,35 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateNewClient(t *testing.T) {
+	client, err := NewClient("John Doe", "test@test.com")
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+	assert.Equal(t, "John Doe", client.Name)
+	assert.Equal(t, "test@test.com", client.Email)
+}
+
+func TestCreateNewClientWhenArgsAreInvalid(t *testing.T) {
+	client, err := NewClient("", "")
+	assert.NotNil(t, err)
+	assert.Nil(t, client)
+}
+
+func TestUpdateClient(t *testing.T) {
+	client, _ := NewClient("John Doe", "test@test.com")
+	err := client.Update("John Doe Jr.", "test@john.com")
+	assert.Nil(t, err)
+	assert.Equal(t, "John Doe Jr.", client.Name)
+	assert.Equal(t, "test@john.com", client.Email)
+}
+
+func TestUpdateClientWithInvalidArgs(t *testing.T) {
+	client, _ := NewClient("John Doe", "test@test.com")
+	err := client.Update("", "test@john.com")
+	assert.Error(t, err, "name is requeired")
+}
